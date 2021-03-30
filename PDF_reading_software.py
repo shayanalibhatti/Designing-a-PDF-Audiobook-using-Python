fd9984f982f5c40b32b03e09b508b22b47a803c7
Shayan Ali Bhatti <bhatt226@d.umn.edu>
2021-03-30 18:06:19 -0500

    Verified working on Ubuntu and created requirements.txt

--- PDF_reading_software.py
+++ PDF_reading_software.py
@@ -1,5 +1,6 @@
-# Import the required module for text to speech conversion 
+# Import the required module for text to speech conversion
 from gtts import gTTS
+import pygame
 from pygame import mixer
 from PIL import Image
 import pytesseract
@@ -9,9 +10,10 @@ import PySimpleGUI as sg
 import tkinter as tk
 import fitz
 
+
 # In this function we get first and last page, which we want the software to read
 def get_text(value):
-        
+
     string = value
     string = string.strip()
     if "-" in string:
@@ -20,7 +22,7 @@ def get_text(value):
     else:
         first_page_number = int(string)
         last_page_number = 0
-        
+
     return first_page_number,last_page_number
 
 def main():
@@ -32,7 +34,7 @@ def main():
         os.makedirs(final_directory)
     print(current_directory)
     print(final_directory)
-        
+
     #### GUI Part #####
 
     # All the stuff inside your window.
@@ -51,17 +53,17 @@ def main():
         pdf_to_read = values[0]
 
         if event in (None, 'Cancel'):	# if user closes window or clicks cancel
-            print("Exitting")            
+            print("Exitting")
             window.close()
             exit()
-    
+
         if event == "Ok":
 
             if values[0] == "":
                 sg.Popup("Enter value", "Enter PDF file to be transcribed ")
             if values[1] == "":
                 sg.Popup("Enter value", "Enter page number(s) to be transcribed")
-            
+
             if values[0]!="" and values[1]!="":
                 for char in values[1]:
                     if char.isdigit()==False:
@@ -70,14 +72,14 @@ def main():
                     else:
                         valid=True
                         break
-        # Break while loop if valid first and last page numbers received    
+        # Break while loop if valid first and last page numbers received
         if valid==True:
             print('You entered ', values[1])
-            break    
+            break
 
     window.close()
     first_page_number,last_page_number = get_text(values[1])
-    
+
     # In this bunch of code, we get permission to delete the folder if it already exists, where we intend to save our PDF images and audio
     image_directory = glob.glob(final_directory)
     for file in os.listdir(final_directory):
@@ -98,7 +100,7 @@ def main():
         pix = page.getPixmap(matrix=mat)
         output = os.path.join(final_directory, r"image_to_read.png")
         pix.writePNG(output)
-        
+
     # If user wants to read range of pages
     else:
         for i in range(first_page_number-1,last_page_number):
@@ -114,27 +116,27 @@ def main():
     print("Done")
 
     # Initialize the Pytesseract OCR software
-    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    # pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-    
+
     mytext = []
 
     # Here we load the image(s) created in Text_to_speech folder and read the text in image via pytesseract Optical Character Recognition (OCR) software
     # thus reading text in images and giving us a string
-    for file in os.listdir(final_directory):    
+    for file in os.listdir(final_directory):
         data = pytesseract.image_to_string(Image.open(os.path.join(final_directory,file)),lang="eng")
         data = data.replace("|","I") # For some reason the image to text translation would put | instead of the letter I. So we replace | with I
         data = data.split('\n')
         mytext.append(data)
 
 
-    # Language in which you want to convert 
+    # Language in which you want to convert
     language = 'en'
 
     print(mytext)
-    
+
     # Here we make sure that the text is read correctly and we read it line by line. Because sometimes, text would end abruptly
-    
+
     newtext= ""
     for text in mytext:
         for line in text:
@@ -150,25 +152,27 @@ def main():
                     newtext = newtext + " " + str(line)
                 else:
                     newtext = newtext + " " + line + "\n"
-            
-    print(newtext)        
-            
-    # Passing the text and language to the engine,  
-    # here we have marked slow=False. Which tells  
-    # the module that the converted audio should  
-    # have a high speed 
-    myobj = gTTS(text=newtext, lang=language, slow=False) 
-      
+
+    print(newtext)
+
+    # Passing the text and language to the engine,
+    # here we have marked slow=False. Which tells
+    # the module that the converted audio should
+    # have a high speed
+    myobj = gTTS(text=newtext, lang=language, slow=False)
+
     # Saving the converted audio in a mp3 file named pdf_audio.mp3
     myobj.save(os.path.join(final_directory,"pdf_audio.mp3"))
 
     # Here we load and play the audio file
+    pygame.init()
     mixer.init()
     mixer.music.load(os.path.join(final_directory,"pdf_audio.mp3"))
     mixer.music.play()
+    pygame.event.wait()
 
 
     ########## GUI END ########
 
 if __name__ == '__main__':
-    main()    
+    main()
